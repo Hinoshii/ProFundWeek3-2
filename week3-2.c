eeab1cc183960c4d1256a446e83304dbd6e75a77
Hinoshii <110216402+Hinoshii@users.noreply.github.com>
2022-08-17 13:23:59 +0700

    Update week3-2.c

--- week3-2.c
+++ week3-2.c
@@ -3,27 +3,19 @@ int main(){
     printf("Enter Number : ");
     int a;
     scanf("%d",&a);
-    int ans=0;
-    for(int i=0;i<=8;i++){
-        ans*=10;
+    int b[8]={0,0,0,0,0,0,0,0};
+    for(int i=0;i<8;i++){
         if(a%2==1){
-            ans+=1;
+            b[i]=1;
             a/=2;
         }
         else{
+            b[i]=0;
             a/=2;
         }
     }
-    int size=0;
-    int k=ans;
-    while(k){
-        k/=10;
-        size++;
-    }
     printf("Binary form = ");
-    int ch=8-size;
-    for(int i=0;i<ch;i++){
-        printf("0");
+    for(int i=0;i<8;i++){
+        printf("%d",b[7-i]);
     }
-    printf("%d",ans);
 }
